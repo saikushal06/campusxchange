eaf9586e2e4722db3a434fe491f38823e0e93595
saikushal06 <saikushal467@gmail.com>
2026-05-12 19:00:29 +0530

    Added Firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_iBBbojfGSMbGBWU-WD6MmdmA-rzMyPA",
+  authDomain: "campusxchange-d1528.firebaseapp.com",
+  projectId: "campusxchange-d1528",
+  storageBucket: "campusxchange-d1528.firebasestorage.app",
+  messagingSenderId: "796660091819",
+  appId: "1:796660091819:web:8d912e7d68b62b6947e0ee"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
